cc283df9858647778efcc78c7539da8c4dcd2470
jordles113 <jordan.t.romero@gmail.com>
2020-11-24 11:37:35 -0600

    updates routes and controllers to use api/v1

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,4 +1,4 @@
-class MessagesController < ApplicationController
+class Api::V1::MessagesController < ApplicationController
 
   def index
     messages = Message.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :users
+  namespace :api do 
+    namespace :v1 do
+      resources :messages
+      resources :users
+    end 
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
