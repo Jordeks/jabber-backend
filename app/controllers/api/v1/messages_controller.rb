868af3030f5e5cd50d8825b7473a006f8e7915a2
MMcClure11 <mcclme02@luther.edu>
2020-11-25 13:20:42 -0600

    adds validation check in message crete action

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -7,8 +7,15 @@ class Api::V1::MessagesController < ApplicationController
   end
 
   def create
-    message = Message.create(message_params)
-    render json: message, status: 200
+    message = Message.new(message_params)
+    if message_params.save
+      render json: message, status: 200
+    else
+      error_resp = {
+        error: message.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
   end
 
   def update
