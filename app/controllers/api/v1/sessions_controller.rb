d3ef683ed5a629d8425a4035b1f2a31a68a17421
MMcClure11 <mcclme02@luther.edu>
2020-11-28 15:58:43 -0600

    verifies user in create action of sessionsController

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create 
-    byebug
+    user = User.find_by(username: params[:session][:username])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      render json: user, status: 200
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
   end
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :username, :name
   has_many :messages
 end
