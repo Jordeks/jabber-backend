ca2b897157d26082148baf4d710e30e531d5c565
MMcClure11 <mcclme02@luther.edu>
2020-11-29 10:34:35 -0600

    updates user_params in usersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,6 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    byebug
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -22,7 +21,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :username, :email, :password, :location)
+    params.permit(:name, :username, :email, :password, :location)
   end
 
 end
